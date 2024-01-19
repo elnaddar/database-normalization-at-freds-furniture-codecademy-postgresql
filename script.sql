639ed88554b438ebb7913a903eaf9d9656b8cb4b
Ahmed Mahdy <am.elnaddar@gmail.com>
2024-01-19 18:11:17 +0200

    created new customers table.

--- script.sql
+++ script.sql
@@ -2,3 +2,8 @@
 SELECT COUNT(DISTINCT(order_id)) "Number of Unique Orders",
        COUNT(DISTINCT(customer_id)) "Number of Unique Customers"
 FROM store;
+
+-- normalizing
+CREATE TABLE customers AS
+SELECT DISTINCT customer_id, customer_phone, customer_email
+FROM store;
